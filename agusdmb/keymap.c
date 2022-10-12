1e797d9f39d91611ae80e43dec877f2e42390550
Agustin Marquez <agusdmb@gmail.com>
2022-10-12 18:21:11 +0200

    WIP

--- agusdmb/keymap.c
+++ agusdmb/keymap.c
@@ -24,7 +24,7 @@ enum layers {
   L_SYMBOLS,
   L_NUMBERS,
   L_OTHER,
-  // L_NUMPAD,
+  L_NUMPAD,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -50,16 +50,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[L_OTHER] = LAYOUT_split_3x6_3(
     KC_F1,          KC_F2,        KC_F3,         KC_F4,         KC_F5,          KC_F6,                KC_F7,        KC_F8,        KC_F9,        KC_F10,       KC_F11,       KC_F12,
     RGB_TOG,        RGB_MOD,      RGB_HUI,       RGB_SAI,       RGB_VAI,        KC_NO,                KC_NO,        KC_KB_POWER,  KC_SLEP,      KC_NO,        KC_NO,        KC_NO,
-    // KC_NO,          RGB_RMOD,     RGB_HUD,       RGB_SAD,       RGB_VAD,        KC_NO,                TG(L_NUMPAD), KC_NO,        KC_NO,        KC_NO,        KC_NO,        KC_NO,
-    KC_NO,          RGB_RMOD,     RGB_HUD,       RGB_SAD,       RGB_VAD,        KC_NO,                KC_NO,        KC_NO,        KC_NO,        KC_NO,        KC_NO,        KC_NO,
+    KC_NO,          RGB_RMOD,     RGB_HUD,       RGB_SAD,       RGB_VAD,        KC_NO,                TG(L_NUMPAD), KC_NO,        KC_NO,        KC_NO,        KC_NO,        KC_NO,
+    // KC_NO,          RGB_RMOD,     RGB_HUD,       RGB_SAD,       RGB_VAD,        KC_NO,                KC_NO,        KC_NO,        KC_NO,        KC_NO,        KC_NO,        KC_NO,
     KC_NO,          KC_NO,        KC_TRNS,       KC_TRNS,       KC_NO,          KC_NO
   ),
-	// [L_NUMPAD] = LAYOUT_split_3x6_3(
- //    KC_NO,          KC_NO,        KC_NO,         KC_NO,         KC_NO,          KC_NO,                KC_PSLS,      KC_P7,        KC_P8,        KC_P9,        KC_PMNS,      KC_BSPC,
- //    TG(L_NUMPAD),   KC_NO,        KC_NO,         KC_NO,         KC_NO,          KC_NO,                KC_PAST,      KC_P4,        KC_P5,        KC_P6,        KC_PPLS,      KC_NO,
- //    KC_NO,          KC_NO,        KC_NO,         KC_NO,         KC_NO,          KC_NO,                KC_P0,        KC_P1,        KC_P2,        KC_P3,        KC_PDOT,      KC_NO,
- //    KC_NO,          KC_NO,        KC_NO,         KC_PEQL,       KC_NO,          KC_0
- //  ),
+	[L_NUMPAD] = LAYOUT_split_3x6_3(
+    KC_NO,          KC_NO,        KC_NO,         KC_NO,         KC_NO,          KC_NO,                KC_PSLS,      KC_P7,        KC_P8,        KC_P9,        KC_PMNS,      KC_BSPC,
+    TG(L_NUMPAD),   KC_NO,        KC_NO,         KC_NO,         KC_NO,          KC_NO,                KC_PAST,      KC_P4,        KC_P5,        KC_P6,        KC_PPLS,      KC_NO,
+    KC_NO,          KC_NO,        KC_NO,         KC_NO,         KC_NO,          KC_NO,                KC_P0,        KC_P1,        KC_P2,        KC_P3,        KC_PDOT,      KC_NO,
+    KC_NO,          KC_NO,        KC_NO,         KC_PEQL,       KC_NO,          KC_0
+  ),
 };
 
 #ifdef OLED_ENABLE
@@ -85,9 +85,9 @@ void oled_render_layer_state(void) {
         case L_OTHER:
             oled_write_ln_P(PSTR("F keys"), false);
             break;
-        // case L_NUMPAD:
-        //     oled_write_ln_P(PSTR("Num Pad"), false);
-        //     break;
+        case L_NUMPAD:
+            oled_write_ln_P(PSTR("Num Pad"), false);
+            break;
         default:
             oled_write_ln_P(PSTR("?????"), false);
     }
--- agusdmb/rules.mk
+++ agusdmb/rules.mk
@@ -6,10 +6,10 @@ OLED_ENABLE       = yes
 OLED_DRIVER       = SSD1306
 LTO_ENABLE        = yes
 
-CONSOLE_ENABLE     = no
-MOUSEKEY_ENABLE    = no
-BOOTMAGIC_ENABLE   = no
-COMBO_ENABLE	   = no
-AUDIO_ENABLE	   = no
-MIDI_ENABLE		   = no
-BLUETOOTH_ENABLE   = no
+# CONSOLE_ENABLE     = no
+# MOUSEKEY_ENABLE    = no
+# BOOTMAGIC_ENABLE   = no
+# COMBO_ENABLE	   = no
+# AUDIO_ENABLE	   = no
+# MIDI_ENABLE		   = no
+# BLUETOOTH_ENABLE   = no
