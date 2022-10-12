4edb3ea7ec8283712bf9fc9202438f7dbc7264e7
Agustin Marquez <agusdmb@gmail.com>
2022-10-12 16:06:51 +0200

    rgb matrix

--- agusdmb/keymap.c
+++ agusdmb/keymap.c
@@ -24,7 +24,7 @@ enum layers {
   L_SYMBOLS,
   L_NUMBERS,
   L_OTHER,
-  L_NUMPAD,
+  // L_NUMPAD,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -36,29 +36,30 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_LGUI,        KC_SPC,       MO(L_SYMBOLS), MO(L_NUMBERS), LCTL_T(KC_ENT), KC_RALT
   ),
 	[L_NUMBERS] = LAYOUT_split_3x6_3(
-    KC_TAB,         KC_1,         KC_2,          KC_3,          KC_4,           KC_5,                 KC_6,         KC_7,         KC_8,         KC_9,         KC_0,         KC_BSPC,
+    KC_TRNS,        KC_1,         KC_2,          KC_3,          KC_4,           KC_5,                 KC_6,         KC_7,         KC_8,         KC_9,         KC_0,         KC_BSPC,
     KC_TRNS,        KC_NO,        KC_MPRV,       KC_MPLY,       KC_MNXT,        KC_NO,                KC_LEFT,      KC_DOWN,      KC_UP,        KC_RGHT,      KC_NO,        KC_RALT,
-    KC_LSFT,        KC_LCTL,      KC_LALT,       KC_NO,         KC_NO,          KC_NO,                KC_HOME,      KC_PGDN,      KC_PGUP,      KC_END,       KC_NO,        KC_LALT,
+    KC_LSFT,        KC_NO,        KC_NO,         KC_NO,         KC_NO,          KC_NO,                KC_HOME,      KC_PGDN,      KC_PGUP,      KC_END,       KC_NO,        KC_LALT,
     KC_TRNS,        KC_TRNS,      MO(L_OTHER),   KC_TRNS,       KC_NO,          KC_NO
   ),
 	[L_SYMBOLS] = LAYOUT_split_3x6_3(
-    KC_NO,          KC_EXLM,      KC_AT,         KC_HASH,       KC_DLR,         KC_PERC,              KC_CIRC,      KC_AMPR,      KC_ASTR,      KC_LPRN,      KC_RPRN,      KC_DEL,
-    KC_NO,          KC_NO,        KC_MUTE,       KC_VOLD,       KC_VOLU,        KC_PSCR,              KC_MINS,      KC_EQL,       KC_LBRC,      KC_RBRC,      KC_BSLS,      KC_GRV,
-    KC_LSFT,        KC_LCTL,      KC_LALT,       KC_BRID,       KC_BRIU,        KC_NO,                KC_UNDS,      KC_PLUS,      KC_LCBR,      KC_RCBR,      KC_PIPE,      KC_TILD,
+    KC_TRNS,        KC_EXLM,      KC_AT,         KC_HASH,       KC_DLR,         KC_PERC,              KC_CIRC,      KC_AMPR,      KC_ASTR,      KC_LPRN,      KC_RPRN,      KC_DEL,
+    KC_TRNS,        KC_NO,        KC_MUTE,       KC_VOLD,       KC_VOLU,        KC_PSCR,              KC_MINS,      KC_EQL,       KC_LBRC,      KC_RBRC,      KC_BSLS,      KC_GRV,
+    KC_LSFT,        KC_NO,        KC_NO,         KC_BRID,       KC_BRIU,        KC_NO,                KC_UNDS,      KC_PLUS,      KC_LCBR,      KC_RCBR,      KC_PIPE,      KC_TILD,
     KC_NO,          KC_NO,        KC_TRNS,       MO(L_OTHER),   KC_TRNS,        KC_TRNS
   ),
 	[L_OTHER] = LAYOUT_split_3x6_3(
     KC_F1,          KC_F2,        KC_F3,         KC_F4,         KC_F5,          KC_F6,                KC_F7,        KC_F8,        KC_F9,        KC_F10,       KC_F11,       KC_F12,
-    KC_NO,          KC_NO,        KC_NO,         KC_NO,         KC_NO,          KC_NO,                KC_NO,        KC_KB_POWER,  KC_SLEP,      KC_NO,        KC_NO,        KC_NO,
-    KC_NO,          KC_NO,        KC_NO,         KC_NO,         KC_NO,          KC_NO,                TG(L_NUMPAD), KC_NO,        KC_NO,        KC_NO,        KC_NO,        KC_NO,
+    RGB_TOG,        RGB_MOD,      RGB_HUI,       RGB_SAI,       RGB_VAI,        KC_NO,                KC_NO,        KC_KB_POWER,  KC_SLEP,      KC_NO,        KC_NO,        KC_NO,
+    // KC_NO,          RGB_RMOD,     RGB_HUD,       RGB_SAD,       RGB_VAD,        KC_NO,                TG(L_NUMPAD), KC_NO,        KC_NO,        KC_NO,        KC_NO,        KC_NO,
+    KC_NO,          RGB_RMOD,     RGB_HUD,       RGB_SAD,       RGB_VAD,        KC_NO,                KC_NO,        KC_NO,        KC_NO,        KC_NO,        KC_NO,        KC_NO,
     KC_NO,          KC_NO,        KC_TRNS,       KC_TRNS,       KC_NO,          KC_NO
   ),
-	[L_NUMPAD] = LAYOUT_split_3x6_3(
-    KC_NO,          KC_NO,        KC_NO,         KC_NO,         KC_NO,          KC_NO,                KC_PSLS,      KC_P7,        KC_P8,        KC_P9,        KC_PMNS,      KC_BSPC,
-    TG(L_NUMPAD),   KC_NO,        KC_NO,         KC_NO,         KC_NO,          KC_NO,                KC_PAST,      KC_P4,        KC_P5,        KC_P6,        KC_PPLS,      KC_NO,
-    KC_NO,          KC_NO,        KC_NO,         KC_NO,         KC_NO,          KC_NO,                KC_P0,        KC_P1,        KC_P2,        KC_P3,        KC_PDOT,      KC_NO,
-    KC_NO,          KC_NO,        KC_NO,         KC_PEQL,       KC_NO,          KC_0
-  ),
+	// [L_NUMPAD] = LAYOUT_split_3x6_3(
+ //    KC_NO,          KC_NO,        KC_NO,         KC_NO,         KC_NO,          KC_NO,                KC_PSLS,      KC_P7,        KC_P8,        KC_P9,        KC_PMNS,      KC_BSPC,
+ //    TG(L_NUMPAD),   KC_NO,        KC_NO,         KC_NO,         KC_NO,          KC_NO,                KC_PAST,      KC_P4,        KC_P5,        KC_P6,        KC_PPLS,      KC_NO,
+ //    KC_NO,          KC_NO,        KC_NO,         KC_NO,         KC_NO,          KC_NO,                KC_P0,        KC_P1,        KC_P2,        KC_P3,        KC_PDOT,      KC_NO,
+ //    KC_NO,          KC_NO,        KC_NO,         KC_PEQL,       KC_NO,          KC_0
+ //  ),
 };
 
 #ifdef OLED_ENABLE
@@ -84,9 +85,9 @@ void oled_render_layer_state(void) {
         case L_OTHER:
             oled_write_ln_P(PSTR("F keys"), false);
             break;
-        case L_NUMPAD:
-            oled_write_ln_P(PSTR("Num Pad"), false);
-            break;
+        // case L_NUMPAD:
+        //     oled_write_ln_P(PSTR("Num Pad"), false);
+        //     break;
         default:
             oled_write_ln_P(PSTR("?????"), false);
     }
--- agusdmb/rules.mk
+++ agusdmb/rules.mk
@@ -1,6 +1,15 @@
-MOUSEKEY_ENABLE = no     # Mouse keys
-RGBLIGHT_ENABLE = no     # Enable WS2812 RGB underlight.
-VIA_ENABLE      = no
-OLED_ENABLE     = yes
-OLED_DRIVER     = SSD1306
-LTO_ENABLE      = yes
+MOUSEKEY_ENABLE   = no     # Mouse keys
+RGBLIGHT_ENABLE   = no     # Enable WS2812 RGB underlight.
+RGB_MATRIX_ENABLE = yes
+VIA_ENABLE        = no
+OLED_ENABLE       = yes
+OLED_DRIVER       = SSD1306
+LTO_ENABLE        = yes
+
+CONSOLE_ENABLE     = no
+MOUSEKEY_ENABLE    = no
+BOOTMAGIC_ENABLE   = no
+COMBO_ENABLE	   = no
+AUDIO_ENABLE	   = no
+MIDI_ENABLE		   = no
+BLUETOOTH_ENABLE   = no
