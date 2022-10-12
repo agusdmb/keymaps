4483168f93fc4d347fd9852d1ee70e985be788f7
Agustin Marquez <agusdmb@gmail.com>
2022-10-12 19:37:48 +0200

    WIP

--- agusdmb/keymap.c
+++ agusdmb/keymap.c
@@ -51,12 +51,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_F1,          KC_F2,        KC_F3,         KC_F4,         KC_F5,          KC_F6,                KC_F7,        KC_F8,        KC_F9,        KC_F10,       KC_F11,       KC_F12,
     RGB_TOG,        RGB_MOD,      RGB_HUI,       RGB_SAI,       RGB_VAI,        KC_NO,                KC_NO,        KC_KB_POWER,  KC_SLEP,      KC_NO,        KC_NO,        KC_NO,
     KC_NO,          RGB_RMOD,     RGB_HUD,       RGB_SAD,       RGB_VAD,        KC_NO,                TG(L_NUMPAD), KC_NO,        KC_NO,        KC_NO,        KC_NO,        KC_NO,
-    // KC_NO,          RGB_RMOD,     RGB_HUD,       RGB_SAD,       RGB_VAD,        KC_NO,                KC_NO,        KC_NO,        KC_NO,        KC_NO,        KC_NO,        KC_NO,
     KC_NO,          KC_NO,        KC_TRNS,       KC_TRNS,       KC_NO,          KC_NO
   ),
 	[L_NUMPAD] = LAYOUT_split_3x6_3(
     KC_NO,          KC_NO,        KC_NO,         KC_NO,         KC_NO,          KC_NO,                KC_PSLS,      KC_P7,        KC_P8,        KC_P9,        KC_PMNS,      KC_BSPC,
-    TG(L_NUMPAD),   KC_NO,        KC_NO,         KC_NO,         KC_NO,          KC_NO,                KC_PAST,      KC_P4,        KC_P5,        KC_P6,        KC_PPLS,      KC_NO,
+    TG(L_NUMPAD),   KC_NO,        KC_NO,         KC_NO,         KC_NO,          KC_NO,                KC_PAST,      KC_P4,        KC_P5,        KC_P6,        KC_PPLS,      KC_NUM,
     KC_NO,          KC_NO,        KC_NO,         KC_NO,         KC_NO,          KC_NO,                KC_P0,        KC_P1,        KC_P2,        KC_P3,        KC_PDOT,      KC_NO,
     KC_NO,          KC_NO,        KC_NO,         KC_PEQL,       KC_NO,          KC_0
   ),
@@ -90,22 +89,6 @@ void oled_render_layer_state(void) {
             break;
         default:
             oled_write_ln_P(PSTR("?????"), false);
-    }
-}
-
-
-void render_bootmagic_status(bool status) {
-    /* Show Ctrl-Gui Swap options */
-    static const char PROGMEM logo[][2][3] = {
-        {{0x97, 0x98, 0}, {0xb7, 0xb8, 0}},
-        {{0x95, 0x96, 0}, {0xb5, 0xb6, 0}},
-    };
-    if (status) {
-        oled_write_ln_P(logo[0][0], false);
-        oled_write_ln_P(logo[0][1], false);
-    } else {
-        oled_write_ln_P(logo[1][0], false);
-        oled_write_ln_P(logo[1][1], false);
     }
 }
 
